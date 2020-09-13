d3ccc60d7cf200c076c316595ac0d10692f7e7ec
higormcosta <higor_c@live.com>
2020-09-13 19:22:37 -0300

    Template dinâmico

--- angular_byAlura/alurabank/app/ts/controllers/NegociacaoController.ts
+++ angular_byAlura/alurabank/app/ts/controllers/NegociacaoController.ts
@@ -4,12 +4,14 @@ class NegociacaoController {
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
 
         this._inputData = <HTMLInputElement> document.querySelector('#data');
         this._inputQuantidade = <HTMLInputElement> document.querySelector('#quantidade');
         this._inputValor = <HTMLInputElement> document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
         
     }
 
@@ -18,12 +20,14 @@ class NegociacaoController {
         event.preventDefault();
         
         const negociacao = new Negociacao(
-            new Date(this._inputValor.value.replace(/-/g, ',')),
+            new Date(this._inputData.value.replace(/-/g, ',')),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
 
         this._negociacoes.adiciona(negociacao);
+
+        this._negociacoesView.update(this._negociacoes);
     }
 
 }
