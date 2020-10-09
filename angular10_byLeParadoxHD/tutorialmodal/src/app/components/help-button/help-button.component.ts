e5425953ee0799f931909d2b24e8148b322ac510
higormcosta <higor_c@live.com>
2020-10-09 16:31:06 -0300

    Adicionado dialog.

--- angular10_byLeParadoxHD/tutorialmodal/src/app/components/help-button/help-button.component.ts
+++ angular10_byLeParadoxHD/tutorialmodal/src/app/components/help-button/help-button.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'help-button',
   templateUrl: './help-button.component.html',
   styleUrls: ['./help-button.component.scss']
 })
-export class HelpButtonComponent implements OnInit {
+export class HelpButtonComponent {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  openDialog() {
+    this.dialog.open(DialogElementsExampleDialog);
   }
 
 }
+
+
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  templateUrl: 'dialog-elements-example-dialog.html',
+})
+export class DialogElementsExampleDialog {}
