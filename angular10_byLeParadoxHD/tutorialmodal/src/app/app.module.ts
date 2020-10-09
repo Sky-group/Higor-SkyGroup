217c6da319da37133733d2f7175564f019c604eb
higormcosta <higor_c@live.com>
2020-10-09 15:34:55 -0300

    Adicionado formulário.

--- angular10_byLeParadoxHD/tutorialmodal/src/app/app.module.ts
+++ angular10_byLeParadoxHD/tutorialmodal/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HelpButtonComponent } from './components/help-button/help-button.component';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpButtonComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular10_byLeParadoxHD/tutorialmodal/src/app/components/help-button/help-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'help-button',
+  templateUrl: './help-button.component.html',
+  styleUrls: ['./help-button.component.scss']
+})
+export class HelpButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
