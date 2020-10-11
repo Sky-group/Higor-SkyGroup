a02238378637bc7163426a68f83782ee14844469
higormcosta <higor_c@live.com>
2020-10-11 02:06:53 -0300

    Inicio da criação do help tooltip.

--- angular10_byLeParadoxHD/tutorialmodal/src/app/directives/help-info.directive.ts
+++ angular10_byLeParadoxHD/tutorialmodal/src/app/directives/help-info.directive.ts
@@ -1,4 +1,4 @@
-import { Component, Directive, ElementRef, HostBinding, HostListener, Input, OnInit } from '@angular/core';
+import { ContentChild, Directive, ElementRef, HostListener, Input, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { HelpDialogComponent } from '../components/help-dialog/help-dialog.component';
 
@@ -10,17 +10,48 @@ export class HelpDirective implements OnInit {
   @Input() tipo: string;
   @Input() info: string;
 
-  constructor(private dialog: MatDialog) {
+  constructor(
+    private dialog: MatDialog, 
+    private renderer: Renderer2, 
+    private elementRef: ElementRef) {
   }
 
   @HostListener('click', ['$event'])
   onClickHandler(event: MouseEvent) {
-    console.log("Click: ", this.tipo, this.info, event);
+
+    if(this.tipo === "modal"){
+      this.dialog.open(HelpDialogComponent,
+        {
+          data: { tipo: this.tipo, info: this.info }
+        });
+    }
     
-    this.dialog.open(HelpDialogComponent,
-      {
-        data: { tipo: this.tipo, info: this.info }
-      });
+  }
+  
+
+  @HostListener('mouseenter', ['$event'])
+  onMouseEnter(event: MouseEvent) {
+
+    if(this.tipo === "tooltip") {
+      const div = this.renderer.createElement('div');
+      const text = this.renderer.createText(this.info);
+      this.renderer.addClass(div, 'tooltip')
+      this.renderer.appendChild(div, text);
+      this.renderer.appendChild(this.elementRef.nativeElement, div);
+    }
+    
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event: MouseEvent) {
+
+    if(this.tipo === "tooltip") {
+      const tooltip = this.elementRef.nativeElement.querySelector('.tooltip');
+      if(tooltip != null) {
+        this.renderer.removeChild(this.elementRef.nativeElement, tooltip);
+      }
+    }
+    
   }
 
   ngOnInit() {
