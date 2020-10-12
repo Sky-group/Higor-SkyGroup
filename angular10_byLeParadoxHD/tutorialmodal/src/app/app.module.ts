b26c1e5aeada921e345f2a63fd24251b9b56b7a9
higormcosta <higor_c@live.com>
2020-10-11 21:44:15 -0300

    Ajustes no tooltip.

--- angular10_byLeParadoxHD/tutorialmodal/src/app/app.module.ts
+++ angular10_byLeParadoxHD/tutorialmodal/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HelpButtonComponent } from './components/help-button/help-button.component';
+import { HelpButtonComponent } from './components/help/help-button/help-button.component';
 
 import { MaterialModule } from './material.module';
 import { HelpDirective } from './directives/help-info.directive';
--- angular10_byLeParadoxHD/tutorialmodal/src/app/directives/help-info.directive.ts
+++ angular10_byLeParadoxHD/tutorialmodal/src/app/directives/help-info.directive.ts
@@ -1,6 +1,6 @@
-import { ContentChild, Directive, ElementRef, HostListener, Input, OnInit, Renderer2, ViewChild, ViewContainerRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { HelpDialogComponent } from '../components/help-dialog/help-dialog.component';
+import { HelpDialogComponent } from '../components/help/help-dialog/help-dialog.component';
 
 @Directive({
   selector: '[helpInfo]'
@@ -18,40 +18,45 @@ export class HelpDirective implements OnInit {
 
   @HostListener('click', ['$event'])
   onClickHandler(event: MouseEvent) {
-
     if(this.tipo === "modal"){
       this.dialog.open(HelpDialogComponent,
         {
           data: { tipo: this.tipo, info: this.info }
         });
     }
-    
   }
   
 
   @HostListener('mouseenter', ['$event'])
   onMouseEnter(event: MouseEvent) {
-
     if(this.tipo === "tooltip") {
-      const div = this.renderer.createElement('div');
-      const text = this.renderer.createText(this.info);
-      this.renderer.addClass(div, 'tooltip')
-      this.renderer.appendChild(div, text);
-      this.renderer.appendChild(this.elementRef.nativeElement, div);
+      this.criaTooltip();
     }
-    
   }
 
   @HostListener('mouseleave', ['$event'])
   onMouseLeave(event: MouseEvent) {
-
     if(this.tipo === "tooltip") {
-      const tooltip = this.elementRef.nativeElement.querySelector('.tooltip');
-      if(tooltip != null) {
-        this.renderer.removeChild(this.elementRef.nativeElement, tooltip);
-      }
+      this.removeTooltip();
     }
-    
+  }
+
+  criaTooltip() {
+    const div = this.renderer.createElement('div');
+    const divTexto = this.renderer.createElement('div');
+    const text = this.renderer.createText(this.info);
+    this.renderer.addClass(div, 'tooltip');
+    this.renderer.addClass(divTexto, 'tooltiptext');
+    this.renderer.appendChild(divTexto, text);
+    this.renderer.appendChild(div, divTexto);
+    this.renderer.appendChild(this.elementRef.nativeElement, div);
+  }
+
+  removeTooltip() {
+    const tooltip = this.elementRef.nativeElement.querySelector('.tooltip');
+    if(tooltip != null) {
+      this.renderer.removeChild(this.elementRef.nativeElement, tooltip);
+    }
   }
 
   ngOnInit() {
