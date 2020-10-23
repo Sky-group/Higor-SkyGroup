41066c3950b55706dcb8b38075a1e4416628ffce
higormcosta <higor_c@live.com>
2020-10-23 10:28:03 -0300

    UPDATE.

--- nodejs_byNodeBr/08-multi-db-postgres/src/tests/postgresStrategy.test.js
+++ nodejs_byNodeBr/08-multi-db-postgres/src/tests/postgresStrategy.test.js
@@ -7,6 +7,10 @@ const MOCK_HEROI_CADASTRAR = {
     nome: 'Gaviao Negro',
     poder: 'flexas'
 }
+const MOCK_HEROI_ATUALIZAR = {
+    nome: 'Batman',
+    poder: 'Vuadô'
+}
 
 describe('Postgres Strategy', function () {
     this.timeout(Infinity)
@@ -18,13 +22,24 @@ describe('Postgres Strategy', function () {
         assert.deepStrictEqual(result, true)
     })
     it('cadastrar', async function () {
-        const result = await context.create(MOCK_HEROI_CADASTRAR)
+        const result = await context.create(MOCK_HEROI_ATUALIZAR)
         delete result.id
-        assert.deepStrictEqual(result, MOCK_HEROI_CADASTRAR)
+        assert.deepStrictEqual(result, MOCK_HEROI_ATUALIZAR)
     })
     it('listar', async function () {
         const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome })
         delete result.id
         assert.deepStrictEqual(result, MOCK_HEROI_CADASTRAR)
+    })
+    it('atualizar', async function () {
+        const [itemAtualizar] = await context.read({ nome: MOCK_HEROI_ATUALIZAR.nome })
+        const novoItem = {
+            ...MOCK_HEROI_ATUALIZAR,
+            nome: 'Mulher Ervilha'
+        }
+        console.log(itemAtualizar)
+        const [result] = await context.update(itemAtualizar.id, novoItem)
+        const [itemAtualizado] = await context.read({ id: itemAtualizar.id })
+        assert.deepStrictEqual(itemAtualizado.nome, novoItem.nome)
     })
 })
