09b097e7b7e14f3b469995a0287d745a58f89ede
higormcosta <higor_c@live.com>
2020-10-29 15:25:49 -0300

    Resolvers

--- angular_byAlura/alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ angular_byAlura/alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -13,13 +13,10 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
 
-  constructor(private photoService: PhotoService, private activateRoute: ActivatedRoute) { }
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    const albumId = this.activateRoute.snapshot.params.albumId;
-    this.photoService
-    .listFromAlbum(albumId)
-    .subscribe(photos => this.photos = photos);
+    this.photos = this.activateRoute.snapshot.data['photos'];
   }
 
 }
