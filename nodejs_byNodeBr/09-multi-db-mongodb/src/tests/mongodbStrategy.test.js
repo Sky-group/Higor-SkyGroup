1d8c0d47edb2ea89e6436734d4cd32e66530c7d0
higormcosta <higor_c@live.com>
2020-10-28 23:25:58 -0300

    Mongoose

---
+++ nodejs_byNodeBr/09-multi-db-mongodb/src/tests/mongodbStrategy.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+const MongoDb = require('./../db/strategies/mongodb')
+const Context = require('./../db/strategies/base/contextStrategy')
+
+const context = new Context(new MongoDb())
+describe('MongoDb testes', function () {
+    this.beforeAll(async function () {
+        await context.connect()
+    })
+    it('verificar conexao', async () => {
+        const result = await context.isConnected()
+        console.log('result', result)
+        const expected = 'Conectado'
+
+        assert.deepStrictEqual(result, expected)
+    })
+})
