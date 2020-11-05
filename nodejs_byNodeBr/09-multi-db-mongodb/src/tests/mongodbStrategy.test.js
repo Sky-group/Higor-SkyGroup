05d6a329fb7adda5c7dea36b5a34ba971e3431db
higormcosta <higor_c@live.com>
2020-11-05 17:49:17 -0300

    MongoDB READ

--- nodejs_byNodeBr/09-multi-db-mongodb/src/tests/mongodbStrategy.test.js
+++ nodejs_byNodeBr/09-multi-db-mongodb/src/tests/mongodbStrategy.test.js
@@ -6,10 +6,15 @@ const MOCK_HEROI_CADASTRAR = {
     nome: 'Mulher Maravilha',
     poder: 'Laço'
 }
+const MOCK_HEROI_DEFAULT = {
+    nome: `Homem Aranha-${Date.now()}`,
+    poder: 'Teia'
+}
 const context = new Context(new MongoDb())
 describe('MongoDb testes', function () {
     this.beforeAll(async function () {
         await context.connect()
+        await context.create(MOCK_HEROI_DEFAULT)
     })
     it('verificar conexao', async () => {
         const result = await context.isConnected()
@@ -21,6 +26,13 @@ describe('MongoDb testes', function () {
     it('cadastrar', async () => {
         const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
         assert.deepStrictEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
+    })
+    it('listar', async () => {
+        const [{nome,poder}] = await context.read({ nome: MOCK_HEROI_DEFAULT.nome })
+        const result = {
+            nome, poder
+        }
+        assert.deepStrictEqual(result, MOCK_HEROI_DEFAULT)
     })
 
 })
