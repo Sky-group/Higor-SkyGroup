d313f906f913e9f5481569368edd03becc99d237
higormcosta <higor_c@live.com>
2020-11-05 17:00:52 -0300

    MongoDB CREATE

--- nodejs_byNodeBr/09-multi-db-mongodb/src/db/strategies/mongodb.js
+++ nodejs_byNodeBr/09-multi-db-mongodb/src/db/strategies/mongodb.js
@@ -25,7 +25,7 @@ class MongoDB extends ICrud {
         
     }
     defineModel() {
-        heroiSchema = new Mongoose.Schema({
+        const heroiSchema = new Mongoose.Schema({
             nome: {
                 type: String,
                 required: true
@@ -51,13 +51,11 @@ class MongoDB extends ICrud {
         const connection = Mongoose.connection
         this._driver = connection
         connection.once('open', () => console.log('database rodando!'))
+        this.defineModel()
     }
 
-    async create(item) {
-        this._herois = await model.create({
-            nome: 'Batman',
-            poder: 'Dinheiro'
-        })
+    create(item) {
+       return this._herois.create(item)
     }
 }
 
--- nodejs_byNodeBr/09-multi-db-mongodb/src/tests/mongodbStrategy.test.js
+++ nodejs_byNodeBr/09-multi-db-mongodb/src/tests/mongodbStrategy.test.js
@@ -2,6 +2,10 @@ const assert = require('assert')
 const MongoDb = require('./../db/strategies/mongodb')
 const Context = require('./../db/strategies/base/contextStrategy')
 
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Mulher Maravilha',
+    poder: 'Laço'
+}
 const context = new Context(new MongoDb())
 describe('MongoDb testes', function () {
     this.beforeAll(async function () {
@@ -14,4 +18,9 @@ describe('MongoDb testes', function () {
 
         assert.deepStrictEqual(result, expected)
     })
+    it('cadastrar', async () => {
+        const { nome, poder } = await context.create(MOCK_HEROI_CADASTRAR)
+        assert.deepStrictEqual({ nome, poder }, MOCK_HEROI_CADASTRAR)
+    })
+
 })
