368d84dd0506f602a792ba5d6dc3036a7fe6f57c
higormcosta <higor_c@live.com>
2021-02-04 15:35:36 -0300

    api PATCH

--- nodejs_byNodeBr/17-api-atualizar/src/tests/apiHeroes.test.js
+++ nodejs_byNodeBr/17-api-atualizar/src/tests/apiHeroes.test.js
@@ -7,16 +7,31 @@ const MOCK_HEROI_CADASTRAR = {
     poder: 'Marreta Bionica'
 }
 
+const MOCK_HEROI_INICIAL = {
+    nome: 'Gavião Negro',
+    poder: 'Mira Bionica'
+}
+
+let MOCK_ID = ''
+
 describe('Suite de testes da API Heroes', function () {
     this.beforeAll(async () => {
         app = await api
+        const result = await app.inject({
+            method: 'POST',
+            url: '/herois',
+            payload: JSON.stringify(MOCK_HEROI_INICIAL)
+        })
+        const dados = JSON.parse(result.payload)
+        MOCK_ID = dados._id
     })
     
     it('listar /herois', async () => {
         const result = await app.inject({
             method: 'GET',
-            url: '/herois?skip=0&limit=10'
+            url: `/herois?skip=0&limit=10`
         })
+
         const dados = JSON.parse(result.payload)
         const statusCode = result.statusCode
         assert.deepStrictEqual(statusCode, 200)
@@ -79,5 +94,41 @@ describe('Suite de testes da API Heroes', function () {
         assert.ok(statusCode === 200)
         assert.notDeepStrictEqual(_id, undefined)
         assert.deepStrictEqual(message, "Heroi cadastrado com sucesso!")
+    })
+    
+    it('atualizar PATCH - /heroi/:id', async () => {
+        const _id = MOCK_ID
+        const expected = {
+            poder: 'Super Mira'
+        }
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.deepStrictEqual(dados.message, 'Heroi atualizado com sucesso!')
+    })
+
+    it('atualizar PATCH - /heroi/:id - nao deve atualizar com id incorreto', async () => {
+        const _id = `601c3da6330a09beea3e096f`
+        const expected = {
+            poder: 'Super Mira'
+        }
+        const result = await app.inject({
+            method: 'PATCH',
+            url: `/herois/${_id}`,
+            payload: JSON.stringify(expected)
+        })
+
+        const statusCode = result.statusCode
+        const dados = JSON.parse(result.payload)
+
+        assert.ok(statusCode === 200)
+        assert.deepStrictEqual(dados.message, 'Não foi possível atualizar!')
     })
 })
